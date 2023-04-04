8c4654d388fe5cadd8d6ca528609d23bec9bc563
samiali3 <87322514+samiali3@users.noreply.github.com>
2023-04-04 11:45:30 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -6,7 +6,8 @@ module "vpc" {
 
   azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
- # public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  map_public_ip_on_launch = false
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
